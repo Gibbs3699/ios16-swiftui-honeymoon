34b1b7e7119f479bce4664c4d5ead140960a52e5
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-03-07 21:15:34 +0700

    init project

---
+++ Honey Moon/Honey_MoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Honey_MoonApp.swift
+//  Honey Moon
+//
+//  Created by TheGIZzz on 7/3/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Honey_MoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
