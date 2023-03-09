fb5f9105af4e6c5b1d308873ffe12a8122406457
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-03-09 16:41:39 +0700

    add utilities folder

---
+++ Honey Moon/Views/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  Honey Moon
+//
+//  Created by TheGIZzz on 9/3/2566 BE.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
