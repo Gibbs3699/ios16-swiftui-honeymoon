d62ac39667202476cc2c61091e84acd8a85378f2
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-03-09 16:34:21 +0700

    add HoneymoonModel

---
+++ Honey Moon/Models/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  Honey Moon
+//
+//  Created by TheGIZzz on 9/3/2566 BE.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
