7ce4f1d94daecba5a776cf0139eb181a391a95c5
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-03-11 16:13:57 +0700

    add HeaderView

---
+++ Honey Moon/Views/HeaderView.swift
@@ -0,0 +1,47 @@
+//
+//  HeaderView.swift
+//  Honey Moon
+//
+//  Created by TheGIZzz on 11/3/2566 BE.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        HStack {
+            Button(action: {
+                print("Infomation!!!!!!")
+            }) {
+                Image(systemName: "info.circle")
+                    .font(.system(size: 24, weight: .regular))
+            }
+            .accentColor(Color.primary)
+            
+            Spacer()
+            
+            Image("logo-honeymoon-pink")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 28)
+            
+            Spacer()
+            
+            Button(action: {
+                print("Guide!!!!!!")
+            }) {
+                Image(systemName: "questionmark.circle")
+                    .font(.system(size: 24, weight: .regular))
+            }
+            .accentColor(Color.primary)
+        }
+        .padding()
+    }
+}
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+            .previewLayout(.sizeThatFits)
+    }
+}
