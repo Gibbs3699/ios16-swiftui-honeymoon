495d9f4a23727a87c4671f3edd12f09b3fa45175
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-03-12 16:10:58 +0700

    add FooterView

---
+++ Honey Moon/Views/FooterView.swift
@@ -0,0 +1,49 @@
+//
+//  FooterView.swift
+//  Honey Moon
+//
+//  Created by TheGIZzz on 12/3/2566 BE.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "xmark.circle")
+                .font(.system(size: 42, weight: .light))
+            
+            Spacer()
+            
+            Button(action: {
+                print("Success!!!!!!")
+            }) {
+                Text("Book Destination".uppercased())
+                    .font(.system(.subheadline, design: .rounded))
+                    .fontWeight(.heavy)
+                    .padding(.horizontal)
+                    .padding(.vertical, 12)
+                    .accentColor(Color.pink)
+                    .background(
+                        Capsule().stroke(Color.pink, lineWidth: 2)
+                    )
+            }
+            .accentColor(Color.primary)
+            
+            Spacer()
+            
+            Image(systemName: "heart.circle")
+                .font(.system(size: 42, weight: .light))
+            
+            
+        }
+        .padding()
+    }
+}
+
+struct FooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FooterView()
+            .previewLayout(.sizeThatFits)
+    }
+}
